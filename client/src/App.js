c75e576bc0730071b89bf9faa24db1d656a92ebb
Rosa Morales <05rosa04@gmail.com>
2021-05-28 21:21:12 -0500

    :sparkles: Init Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div>
-      <p>Hello 🍸</p>
-    </div>
+    <Router>
+      <MenuBar/>
+      <Route exact path="/" component={ Home } />
+      <Route exact path="/login" component={ Login } />
+      <Route exact path="/register" component={ Register } />
+    </Router>
   );
 }
 
